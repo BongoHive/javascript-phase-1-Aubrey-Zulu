610cb76db334dd372abfa6b3cbb4a002f1c6e2ab
Aubrey-Zulu <aubreydarious@gmail.com>
2019-06-09 01:57:22 +0200

    Assignment completed

--- variables.js
+++ variables.js
@@ -1,1 +1,34 @@
 // you solution should be here
+let birthYear = 1993;
+let futureYear = 2040;
+
+/**
+ *
+ * Solution Number 1
+ * @param {*} birthYear
+ * @param {*} futureYear
+ */
+function calculateFutureAge(birthYear, futureYear) {
+    // the age variable here
+    let age = futureYear - birthYear;
+    console.log('I will be either ' + age + ' or ' + (age - 1))
+} 
+// calling the calculateFutureAge function
+calculateFutureAge(birthYear, futureYear);
+
+let age = 26;
+let maxAge = 120;
+let padDay = 4;
+
+function calculateTotalNeeded(peyDay, maxAge, age) {
+    let totalNeeded = (peyDay * 365) * (maxAge - age);
+    console.log('You will need ' + totalNeeded + ' cups of tea to last you until the ripe old age of ' + maxAge);
+}
+
+calculateTotalNeeded(peyDay, maxAge, age)
+
+
+
+
+
+
