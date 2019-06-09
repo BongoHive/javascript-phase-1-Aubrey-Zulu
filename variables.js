786df12e3c5a37e88c17fd1b37cbef73f4bcc161
Aubrey-Zulu <aubreydarious@gmail.com>
2019-06-09 08:49:59 +0200

    Refactor code

--- variables.js
+++ variables.js
@@ -1,38 +1,20 @@
 // you solution should be here
 let birthYear = 1993;
 let futureYear = 2040;
+let age1 = futureYear - birthYear;
+let text = 'I will be either ' + age1 + ' or ' + (age1 - 1);
 
-/**
- *
- * Solution Number 1
- * @param {*} birthYear
- * @param {*} futureYear
- */
-function calculateFutureAge(birthYear, futureYear) {
-    // the age variable here
-    let age = futureYear - birthYear;
-    console.log('I will be either ' + age + ' or ' + (age - 1))
-} 
-// calling the calculateFutureAge function
-calculateFutureAge(birthYear, futureYear);
+console.log(text);
+
 
 
 
 // Solution Number 2;
 
-let age = 26;
+let age2 = 26;
 let maxAge = 120;
 let pedDay = 4;
 
-function calculateTotalNeeded(peyDay, maxAge, age) {
-    let totalNeeded = (peyDay * 365) * (maxAge - age);
-    console.log('You will need ' + totalNeeded + ' cups of tea to last you until the ripe old age of ' + maxAge);
-}
-
-calculateTotalNeeded(peyDay, maxAge, age)
-
-
-
-
-
-
+let totalNeeded = (pedDay * 365) * (maxAge - age2);
+let messege = 'You will need ' + totalNeeded + ' cups of tea to last you until the ripe old age of ' + maxAge;
+console.log(messege);
